1cff81e323c0133000cca11490e1402b646b6989
Antoine Marechal <antoine.marechal2@essec.edu>
2017-08-05 13:15:24 +0000

    Envoi de mails via SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,9 @@ Paperclip::Attachment.default_options.merge!({
  :path => ':id/:style/:filename'
  })
  
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.default_url_options = { :host => 'altr.herokuapp.com', :protocol => 'https'}
+ 
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

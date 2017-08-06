d92099d0ef091609feead55ba97a7eb468be36cc
Antoine Marechal <antoine.marechal2@essec.edu>
2017-08-06 13:13:53 +0000

    Bouton d'inscription avec Facebook

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,14 @@
 module ApplicationHelper
     
     def avatar_url(user)
+    if user.image
+       user.image
+    else
+       user.avatar.url
+    end
+    end
+    
+    def avatar_url(user)
         if user.avatar.present?
             user.avatar.url
         else

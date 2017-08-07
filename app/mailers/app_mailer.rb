b67c827c43dd267f79ae7f589bc17c792773fab4
Antoine Marechal <antoine.marechal2@essec.edu>
2017-08-07 13:56:40 +0000

    Petite correction du mailer

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -3,6 +3,6 @@ class AppMailer < ActionMailer::Base
        @user = User.find(problem.user_id)
        @resolution = resolution
        @problem = problem
- mail(from: 'AltR <no-reply@altr.com>', to: @user.email, subject: "Quelqu'un veut vous aider à résoudre votre problème !")
+ mail(from: 'AltR <no-reply@alt-r.fr>', to: @user.email, subject: "Quelqu'un veut vous aider à résoudre votre problème !")
     end
 end

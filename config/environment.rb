e31925ef5ba43ba04b465185d543c83d909315ad
Antoine Marechal <antoine.marechal2@essec.edu>
2017-08-07 18:21:06 +0000

    marche stp

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,6 @@ ActionMailer::Base.smtp_settings = {
  :authentication => :plain,
  :user_name => ENV['SENDGRID_USERNAME'],
  :password => ENV['SENDGRID_PASSWORD'],
- :domain => 'alt-r.fr',
+ :domain => 'app.alt-r.fr',
  :enable_starttls_auto => true
  }

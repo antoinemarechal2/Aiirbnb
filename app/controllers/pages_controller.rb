9a72195d7a0a58a1e4ffe3ec0558c4de7e22ba48
Antoine Marechal <antoine.marechal2@essec.edu>
2017-08-12 14:34:27 +0000

    méthode search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,23 @@ class PagesController < ApplicationController
         def home
             @problems= Problem.order("RANDOM()").limit(6)
         end
+
+def search
+    if params[:search].present? && params[:search].strip !=""
+        session[:altr_search] = params[:search]
+    end
+
+    arrResult = Array.new
+    
+    if session[:altr_search] && session[:altr_search] != ""
+        @problems_category = Problem.where(active: true)
+    else
+        @problems_category = Problem.where(active: true).all
+    end
+
+@search = @problems_category.ransack(params[:q])
+@problems = @search.result
+
+@arrProblems = @problem.to_a
+end 
 end

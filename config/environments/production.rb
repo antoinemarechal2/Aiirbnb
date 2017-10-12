9553c71cadc7b19fa2ce729d8b3c22708265c183
Cyril Blaecke <cyril@dilolabs.fr>
2017-10-12 14:10:10 +0200

    Fix action_mailer config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,15 +6,17 @@ Rails.application.configure do
   })
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {
-    protocol:             'https',
-    host:                 'www.alt-r.fr',
+  config.action_mailer.smtp_settings = {
     address:              'smtp.alt-r.fr',
     port:                 587,
     user_name:            'contact@alt-r.fr',
     password:             'mHaqnh9BWWQrc9prWHNbKd&HU4',
     authentication:       'login',
     enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = {
+    protocol:             'https',
+    host:                 'www.alt-r.fr',
   }
 
   # Settings specified here will take precedence over those in config/application.rb.
